1fe7fe03128372203fe384084479f08d1f51ae9e
ART <popugajarcik@gmail.com>
2025-07-06 21:44:33 +0300

    icon load fix

--- src/components/customUI/IconButton.vue
+++ src/components/customUI/IconButton.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { Button } from '@/components/ui/button';
 import { Icon } from '@iconify/vue';
+import { onBeforeMount } from 'vue'
+import { loadIcons } from '@iconify/vue'
 
 const props = defineProps({
     icon: String
+})
+
+onBeforeMount(() => {
+  loadIcons([props.icon])
 })
 </script>
 
